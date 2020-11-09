ccfe80a2f6f854a54e1526a093472e4b5bc91564
Lanzhou Jiang <lzjiang99@gmail.com>
2020-11-09 21:50:21 +1100

    commented the last test case.

--- conways-game-of-life/Tick.cs
+++ conways-game-of-life/Tick.cs
@@ -4,7 +4,6 @@ namespace conways_game_of_life
     {
         public static int[][] Evolve(int[][] input)
         {
-            
             return new[] {
                 new[]{0,0,0},
                 new[]{0,0,0},

b50b97fbc6f21a02f290a27dea383274374995c4
Lanzhou Jiang <lzjiang99@gmail.com>
2020-11-10 13:17:37 +1100

    refactored production code, add a new constructor in Board class, add a new test case in TickTests, red bar

--- conways-game-of-life-tests/TickTests.cs
+++ conways-game-of-life-tests/TickTests.cs
@@ -57,5 +57,39 @@ namespace conways_game_of_life_tests
             }
 
         }
+        
+        [Fact]
+        public void EvolveShould_ReturnAWorldOfDeadCells_WhenInputWorldHasOneLiveCellInMiddle()
+        {
+            //arrange
+            State[][] initial = new[]
+            {
+                new[] {State.Dead, State.Dead, State.Dead},
+                new[] {State.Dead, State.Live, State.Dead},
+                new[] {State.Dead, State.Dead, State.Dead}
+            };
+            
+            var input = new Board(initial);
+
+            var result = new[] {
+                new[]{State.Dead,State.Dead,State.Dead},
+                new[]{State.Dead,State.Dead,State.Dead},
+                new[]{State.Dead, State.Dead,State.Dead}
+            };
+          
+            //act
+            Tick.Evolve(input);
+          
+            //assert
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    var actualCellValue = input.GetCell(i, j).State;
+                    var expectedCellValue = result[i][j];
+                    Assert.Equal(expectedCellValue,actualCellValue);
+                }
+            }
+        }
     }
 }
--- conways-game-of-life/Board.cs
+++ conways-game-of-life/Board.cs
@@ -11,6 +11,13 @@ namespace conways_game_of_life
             CreateBoard();
         }
         
+        public Board(State[][] states)
+        {
+            Row = states.GetLength(0);
+            Column = states[0].Length;
+            CreateBoard(states);
+        }
+        
         private void CreateBoard()
         {
             _squares = new Cell[Row, Column];
@@ -22,6 +29,18 @@ namespace conways_game_of_life
                 }
             }
         }
+        
+        private void CreateBoard(State[][] states)
+        {
+            _squares = new Cell[Row, Column];
+            for (var x = 0; x < Row; x++)
+            {
+                for (var y = 0; y < Column; y++)
+                {
+                    _squares[x, y] = new Cell {State = states[x][y]};
+                }
+            }
+        }
 
         public int Row { get; }
         public int Column { get; }
@@ -29,6 +48,11 @@ namespace conways_game_of_life
         public Cell GetCell(int x, int y)
         {
             return _squares[x, y];
+        }
+        
+        public void ChangeCell(int x, int y)
+        { 
+            _squares[x, y].ChangeState();
         }
     }
 }

f008d2be00f9356bf4ccba46268223568f6c3a33
Lanzhou Jiang <lzjiang99@gmail.com>
2020-11-10 12:42:30 +1100

    add a new test case in TickTests class, green bar.

--- conways-game-of-life-tests/TickTests.cs
+++ conways-game-of-life-tests/TickTests.cs
@@ -29,6 +29,32 @@ namespace conways_game_of_life_tests
                     Assert.Equal(expectedCellValue,actualCellValue);
                 }
             }
+        }
+        
+        [Fact]
+        public void EvolveShould_ReturnAWorldOfDeadCells_WhenInputWorldCellsAreDeadRegardlessOfWorldSize()
+        {
+            //arrange
+            var initial = new Board(3,4);
+            
+            var result = new[] {
+                new[]{State.Dead,State.Dead,State.Dead, State.Dead},
+                new[]{State.Dead,State.Dead,State.Dead, State.Dead},
+                new[]{State.Dead, State.Dead,State.Dead, State.Dead}
+            };
+
+            //act
+            Tick.Evolve(initial);
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    var actualCellValue = initial.GetCell(i, j).State;
+                    var expectedCellValue = result[i][j];
+                    Assert.Equal(expectedCellValue,actualCellValue);
+                }
+            }
 
         }
     }

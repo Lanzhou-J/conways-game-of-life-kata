14d04e56c134ef70d45d7475a48e27d63219233b
Lanzhou Jiang <lzjiang99@gmail.com>
2020-11-12 23:53:36 +1100

    refactored test code, 12/12 tests passed, green bar

--- conways-game-of-life-tests/TickTests.cs
+++ conways-game-of-life-tests/TickTests.cs
@@ -104,7 +104,9 @@ namespace conways_game_of_life_tests
             };
             var input = new Generation(cells);
             var rule = new Rule();
+            Assert.Equal(1, input.GenerationCount);
             var result = Tick.GenerateNewGeneration(input,rule);
+            Assert.Equal(2, input.GenerationCount);
             for (int i = 0; i < input.Row; i++)
             {
                 for (int j = 0; j < input.Column; j++)

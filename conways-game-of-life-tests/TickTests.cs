f7b035fe0b7c29bec9a8faa3abad782d8786f341
Lanzhou Jiang <lzjiang99@gmail.com>
2020-11-12 17:28:04 +1100

    add new test in TickTests, green bar

--- conways-game-of-life-tests/TickTests.cs
+++ conways-game-of-life-tests/TickTests.cs
@@ -40,5 +40,28 @@ namespace conways_game_of_life_tests
             var result = Tick.GenerateNewGeneration(input);
             Assert.Equal(2, result.GenerationCount);
         }
+        
+        [Fact]
+        public void GenerateNewGenerationShould_ReturnAGenerationWithSameCellStates_WhenNoRuleArgument()
+        {
+            var deadCell = new Cell();
+            var liveCell = new Cell();
+            liveCell.ChangeState();
+            var cells = new[] {
+                new[]{deadCell, liveCell},
+                new []{deadCell, liveCell}
+            };
+            var input = new Generation(cells);
+            var result = Tick.GenerateNewGeneration(input);
+            for (int i = 0; i < input.Row; i++)
+            {
+                for (int j = 0; j < input.Column; j++)
+                {
+                    var expectState = input.Cells[i][j].State;
+                    var actualState = result.Cells[i][j].State;
+                    Assert.Equal(expectState,actualState);
+                }
+            }
+        }
     }
 }

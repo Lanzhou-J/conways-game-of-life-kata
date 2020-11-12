dbd83a1b7995c5f9c56c1b742436bf6a04dec303
Lanzhou Jiang <lzjiang99@gmail.com>
2020-11-12 23:20:02 +1100

    add new test case in RuleTests class to test CellRule 2

---
+++ conways-game-of-life-tests/RuleTests.cs
@@ -0,0 +1,19 @@
+using System;
+using conways_game_of_life;
+using Xunit;
+
+namespace conways_game_of_life_tests
+{
+    public class RuleTests
+    {
+        [Fact]
+        public void IsOvercrowdingForLiveCellShould_ReturnTrue_WhenLiveNeighboursOverThree()
+        {
+            var liveNeighbours = 4;
+            var rule = new Rule();
+            var result = rule.IsOvercrowdingForLiveCell(liveNeighbours);
+            Assert.True(result);
+        }
+
+    }
+}
--- conways-game-of-life/Rule.cs
+++ conways-game-of-life/Rule.cs
@@ -1,7 +1,23 @@
+using System;
+
 namespace conways_game_of_life
 {
     public class Rule
     {
+
+        public bool IsOvercrowdingForLiveCell(int liveNeighbours)
+        {
+            return liveNeighbours > 3;
+        }
         
+        public bool IsUnderpopulationForLiveCell(int liveNeighbours)
+        {
+            return liveNeighbours < 2;
+        }
+        
+        public bool IsGreatConditionForDeadCell(int liveNeighbours)
+        {
+            return liveNeighbours == 3;
+        }
     }
 }

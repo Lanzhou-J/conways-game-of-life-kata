53e85fe695757d18774e045caa52b111e5909298
Lanzhou Jiang <lzjiang99@gmail.com>
2020-11-12 16:53:56 +1100

    add a new test case in GenerationTests class, add logic in GenerateNewGeneration function, green bar

--- conways-game-of-life-tests/GenerationTests.cs
+++ conways-game-of-life-tests/GenerationTests.cs
@@ -1,9 +1,25 @@
+using conways_game_of_life;
 using Xunit;
 
 namespace conways_game_of_life_tests
 {
     public class GenerationTests
     {
-       
+        [Fact]
+        public void GenerateNewGenerationShould_ReturnAGenerationWithCountPlusOne_WhenNoRuleArgument()
+        {
+            var deadCell = new Cell();
+            var liveCell = new Cell();
+            liveCell.ChangeState();
+            Assert.Equal(State.Live,liveCell.State);
+            Cell[][] cells = new Cell[][]
+            {
+                new[]{deadCell,deadCell},
+                new[]{deadCell,deadCell}};
+            var generation = new Generation(cells);
+            Assert.Equal(1, generation.GenerationCount);
+            var result = generation.GenerateNewGeneration();
+            Assert.Equal(2,result.GenerationCount);
+        }
     }
 }

d094f706c17a8b2474fbb92ef5a2347a45c89496
Lanzhou Jiang <lzjiang99@gmail.com>
2020-11-17 19:22:54 +1100

    adding a new test for testing GetNeighbours function, when cell is on the border of generation grid

--- conways-game-of-life-tests/GenerationTests.cs
+++ conways-game-of-life-tests/GenerationTests.cs
@@ -133,6 +133,25 @@ namespace conways_game_of_life_tests
             Assert.Equal(8, deadNeighbours.Count);
             Assert.Empty(liveNeighbours);
         }
+        
+        [Fact]
+        public void GetNeighboursShould_ReturnAListWith8ells_WhenTheGenerationIs3times4_AndCellIsOnTheBorder()
+        {
+            var cellStates = new[] {
+                new[]{_dead, _dead, _dead, _dead},
+                new[]{_dead, _live, _dead, _dead},
+                new[]{_dead, _dead, _dead, _dead}
+            };
+            
+            var generation = new Generation(cellStates,1);
+            var cell = generation.GetCell(0,0);
+            var result = generation.GetNeighbours(cell);
+            Assert.Equal(8,result.Count);
+            var liveNeighbours = result.FindAll(x => x.State.Equals(State.Live));
+            var deadNeighbours = result.FindAll(x => x.State.Equals(State.Dead));
+            Assert.Equal(7, deadNeighbours.Count);
+            Assert.Single(liveNeighbours);
+        }
 
     }
 }

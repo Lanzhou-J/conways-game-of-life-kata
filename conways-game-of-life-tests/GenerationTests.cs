fd36a1afef310d42a03841a1b012af55356b7617
Lanzhou Jiang <lzjiang99@gmail.com>
2020-11-17 17:16:11 +1100

    add a new test case, green bar

--- conways-game-of-life-tests/GenerationTests.cs
+++ conways-game-of-life-tests/GenerationTests.cs
@@ -5,12 +5,14 @@ namespace conways_game_of_life_tests
 {
     public class GenerationTests
     {
+        private State _dead = State.Dead;
+        private State _live = State.Live;
         [Fact]
         public void GetCellStateShould_ReturnStateOfCell_WhenCellIndexWasProvided()
         {
             var cellStates = new[] {
-                new[]{State.Dead, State.Live},
-                new []{State.Dead, State.Live}
+                new[]{_dead, _live},
+                new []{_dead, _live}
             };
             var generation = new Generation(cellStates,1);
             var result = generation.GetCellState(0, 0);
@@ -21,8 +23,8 @@ namespace conways_game_of_life_tests
         public void GetNeighboursShould_ReturnEmptyList_WhenNoArgument()
         {
             var cellStates = new[] {
-                new[]{State.Dead, State.Live},
-                new []{State.Dead, State.Live}
+                new[]{_dead, _live},
+                new []{_dead, _live}
             };
             var generation = new Generation(cellStates,1);
             var result = generation.GetNeighbours();
@@ -33,7 +35,7 @@ namespace conways_game_of_life_tests
         public void GetNeighboursShould_ReturnEmptyList_WhenThereIsOnlyOneCell()
         {
             var cellStates = new[] {
-                new[]{State.Dead}
+                new[]{_dead}
                 };
             var generation = new Generation(cellStates,1);
             var firstCell = generation.Cells[0][0];
@@ -44,22 +46,36 @@ namespace conways_game_of_life_tests
         [Fact]
         public void GetNeighboursShould_ReturnAListWithOneCell_WhenThereAreTwoCells()
         {
-            var dead = State.Dead;
-            var live = State.Live;
             var cellStates = new[] {
-                new[]{dead, live},
+                new[]{_dead, _live},
             };
             var generation = new Generation(cellStates,1);
             var firstCell = generation.Cells[0][0];
             var result = generation.GetNeighbours(firstCell);
             Assert.NotEmpty(result);
-            Assert.Equal(live,result[0].State);
+            Assert.Equal(_live,result[0].State);
 
             var secondCell = generation.Cells[0][1];
             var secondResult = generation.GetNeighbours(secondCell);
             Assert.NotEmpty(result);
-            Assert.Equal(dead,secondResult[0].State);
-            
+            Assert.Equal(_dead,secondResult[0].State);
+        }
+        
+        [Fact]
+        public void GetNeighboursShould_ReturnAListWith3Cells_WhenThereAreFourCells()
+        {
+            var cellStates = new[] {
+                new[]{_dead, _live},
+                new[]{_dead, _dead}
+            };
+            var generation = new Generation(cellStates,1);
+            var cell = generation.Cells[0][1];
+            var result = generation.GetNeighbours(cell);
+            Assert.Equal(3,result.Count);
+            foreach (var item in result)
+            {
+                Assert.Equal(_dead, item.State);
+            }
         }
 
     }
